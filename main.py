7e45b974edbda5dc5bf77ae3b3c6d767db761dee
Arminaz <arminaz321@hotmail.ca>
2025-02-25 05:16:17 -0700

    Added Password Generator and finished the Project

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 # ---------------------------- IMPORTS ------------------------------- #
 from tkinter import *
 from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+            'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
+            'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = randint(8, 10)
+    nr_symbols = randint(2, 4)
+    nr_numbers = randint(2, 4)
+
+    password_list = []
+
+    password_list.extend(choice(letters) for _ in range(nr_letters))
+    password_list.extend(choice(symbols) for _ in range(nr_symbols))
+    password_list.extend(choice(numbers) for _ in range(nr_numbers))
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_input.delete(0, END)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -12,7 +40,7 @@ def save():
     password = password_input.get()
 
     if len(website) == 0 or len(username) == 0 or len(password) == 0:
-        messagebox.showerror(title="Oops" message="Please do not leave any empty fields!")
+        messagebox.showerror(title="Oops", message="Please do not leave any empty fields!")
         return
 
     is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered:\nEmail: {username}\nPassword: {password}\n Is it ok to save?")
@@ -54,7 +82,7 @@ email_user_input = Entry(width=39)
 password_input = Entry(width=21)
 
 # # Buttons
-generate_password = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 add_button = Button(width=36, text="Add", command=save)
 
 # # Grid Placement
@@ -66,7 +94,7 @@ email_user_input.grid(column=2, row=3, columnspan=2)
 
 password_label.grid(column=1, row=4)
 password_input.grid(column=2, row=4)
-generate_password.grid(column=3, row=4)
+generate_password_button.grid(column=3, row=4)
 
 add_button.grid(column=2, row=5, columnspan=2)
 
